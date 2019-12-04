3e74d9366c310cb0e12536942cf896f20f44d353
krisdw <chenjinwen77@foxmail.com>
2019-12-04 16:42:42 +0800

    added prioerity queue

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
 'use strict'
+import PriorityQueue from './ts/cores/queue/priorityQueue'
+
+let queue = new PriorityQueue()
+queue.enqueue('a', 1)
+queue.enqueue('b', 2)
+queue.enqueue('c', 3)
+
+console.log(queue.front())
+console.log(queue.back())
+queue.print()
