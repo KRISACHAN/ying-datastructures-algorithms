32e90f11177fea6893bf7827b87c1b47f0e8df65
krisdw <chenjinwen77@foxmail.com>
2021-01-30 11:44:01 +0800

    fix(type): 修复类型报错

--- src/core/algorithms/search/blockSearch.ts
+++ src/core/algorithms/search/blockSearch.ts
@@ -6,10 +6,10 @@ import {
     DOES_NOT_EXIST,
 } from 'core/utils'
 
-export default class BlockSearch<number> {
+export default class BlockSearch {
     protected compareFn: ICompareFunction<number> = defaultCompare
-    private blocks: number[] = []
-    constructor(list: T[] = [], gap: number = 3) {
+    private blocks: any[] = []
+    constructor(list: number[] = [], gap: number = 3) {
         list = list.sort((a, b) => a - b)
         if (gap <= 0) {
             throw new Error('list gap must be a positive integer !')
@@ -17,7 +17,7 @@ export default class BlockSearch<number> {
         if (!list.length) {
             return
         }
-        let newList: number[] = []
+        let newList: any[] = []
         let blockIndex: number = 0
         // 先按着间隙分好数列元素，然后每一块加上key，最后再进行排序
         for (let i: number = 0, len: number = list.length; i < len; ++i) {
@@ -34,7 +34,7 @@ export default class BlockSearch<number> {
         }
         this.blocks = newList.sort((a, b) => a.key - b.key)
     }
-    insert(value: number): BlockSearch<number> {
+    insert(value: number): BlockSearch {
         let targetIndex: number = this.blocks.findIndex(
             block => value < block.key,
         )
@@ -42,12 +42,12 @@ export default class BlockSearch<number> {
         this.blocks[targetIndex].key = value
         return this
     }
-    remove(value: number): BlockSearch<number> {
-        const resPos: number[] | -1 = this.search(value)
+    remove(value: number): BlockSearch {
+        const resPos: any = this.search(value)
         if (resPos !== DOES_NOT_EXIST) {
             const key = this.blocks[resPos[0]].key
             this.blocks[resPos[0]] = this.blocks[resPos[0]].filter(
-                data => data !== value,
+                (data: number) => data !== value,
             )
             if (!this.blocks[resPos[0]].length) {
                 this.blocks = this.blocks.filter(block => block.length > 0)
@@ -58,14 +58,14 @@ export default class BlockSearch<number> {
         }
         return this
     }
-    search(value: number): number {
+    search(value: number): number[] | number {
         let blockIndex: number =
             this.blocks.findIndex(block => block.key > value) - 1
         if (blockIndex < 0) {
             return DOES_NOT_EXIST
         }
         let resIndex: number = this.blocks[blockIndex].findIndex(
-            block => block === value,
+            (block: number) => block === value,
         )
         if (resIndex < 0) {
             return DOES_NOT_EXIST
